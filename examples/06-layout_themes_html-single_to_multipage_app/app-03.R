9b224d9c59c4b434d4cd39b0f0b68a6a9e6e9b1a
Russ Hyde <russ.hyde.data@gmail.com>
2021-03-23 16:05:29 +0000

    add separate tabPanels for answer-table and word-cloud

---
+++ examples/06-layout_themes_html-single_to_multipage_app/app-03.R
@@ -0,0 +1,76 @@
+# Plot a wordcloud to summarise the tags that a stack-overflow user tends to
+# answer/ask about
+
+# Made from app-02.R
+# - separated wordcloud and table into separate tabPanels
+
+library(dplyr)
+library(shiny)
+library(wordcloud)
+library(stackr)
+
+# Helper functions
+
+format_answer_score_table <- function(df) {
+  df %>%
+    dplyr::select(tag_name, answer_count, answer_score) %>%
+    dplyr::filter(answer_count > 0) %>%
+    dplyr::arrange(dplyr::desc(answer_score))
+}
+
+make_word_cloud <- function(df) {
+  with(
+    df,
+    wordcloud::wordcloud(
+      words = tag_name,
+      freq = answer_score,
+      min.freq = 0,
+      colors = RColorBrewer::brewer.pal(6, "Purples")[-1],
+      scale = c(10, 0.5)
+    )
+  )
+}
+
+# Define UI for application that plots information about a user's stack
+# overflow presence
+
+ui <- fluidPage(
+
+  # Application title
+  titlePanel("Stack Overflow: User Statistics"),
+
+  # Sidebar for selecting which Stack Overflow user's data is presented
+  sidebarLayout(
+    sidebarPanel(
+      textInput("user_id", "Select a Stack Overflow user ID:", "1845650")
+    ),
+
+    # Show a plot of a wordcloud and table of the user's answer-tags
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Word Cloud", plotOutput("word_cloud")),
+        tabPanel("Answer Table", tableOutput("answer_table"))
+      )
+    )
+  )
+)
+
+# Define server logic required to obtain data from stack-overflow and draw a
+# wordcloud
+
+server <- function(input, output) {
+  stack_data <- reactive(
+    stackr::stack_users(input[["user_id"]], "top-tags"),
+  )
+
+  output$word_cloud <- renderPlot(
+    make_word_cloud(stack_data())
+  )
+
+  output$answer_table <- renderTable(
+    format_answer_score_table(stack_data())
+  )
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
