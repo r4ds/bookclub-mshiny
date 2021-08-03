b16861e26a51154c75aad8c6862190d4eb26feaa
Collin K. Berke, Ph.D <32435546+collinberke@users.noreply.github.com>
2021-08-03 10:48:06 -0500

    Adding/updating material, introducing git and Chapter 1 (#66)
    * Added new content
    
    * Added new content
    
    * Added material on git workflows
    
    This commit contains material overivewing the basic uses of git.
    
    * Embedded meeting vids and chat logs
    
    * Updated schedule
    
    * Added video links and chat log
    
    * Added Ryan S to the schedule

---
+++ examples/01-your_first_shiny_app/app-data-example-final.R
@@ -0,0 +1,25 @@
+library(shiny)
+
+ui <- fluidPage(
+  selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
+  verbatimTextOutput("summary"),
+  tableOutput("table")
+)
+
+server <- function(input, output, session) {
+  # Create a reactive expression
+  dataset <- reactive({
+    get(input$dataset, "package:datasets")
+  })
+  
+  output$summary <- renderPrint({
+    # Use a reactive expression by calling it like a function
+    summary(dataset())
+  })
+  
+  output$table <- renderTable({
+    dataset()
+  })
+}
+
+shinyApp(ui, server)
