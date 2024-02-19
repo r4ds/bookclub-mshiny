7419c2c7e405140c1ec3bc9663cd10a8f53d13c8
Angel Esteban Feliz <84166329+AngelFelizR@users.noreply.github.com>
2024-02-19 10:34:50 -0400

    extending presentation (#97)

--- examples/14-the-reactive-graph/app.R
+++ examples/14-the-reactive-graph/app.R
@@ -9,22 +9,29 @@
 
 library(shiny)
 ui <- fluidPage(
-    numericInput("a", "a", value = 10),
-    numericInput("b", "b", value = 1),
-    numericInput("c", "c", value = 1),
-    plotOutput("x"),
-    tableOutput("y"),
-    textOutput("z")
+  numericInput("a", "Range mid point", value = 10),
+  numericInput("b", "Sample size", value = 1),
+  numericInput("c", "Times sample size", value = 1),
+  br(),
+  h4("Sampled data from the range"),
+  plotOutput("x"),
+  h4("Highest number in the sampled data"),
+  tableOutput("y"),
+  h4("Times sample size"),
+  textOutput("z")
 )
 
 server <- function(input, output, session) {
-    rng <- reactive(input$a * 2,label = "rng")
-    smp <- reactive(sample(rng(), input$b, replace = TRUE), label = "smp")
-    bc <- reactive(input$b * input$c, label = "bc")
-    
-    output$x <- renderPlot(hist(smp()))
-    output$y <- renderTable(max(smp()))
-    output$z <- renderText(bc())
+  rng <- reactive(input$a * 2,
+                  label = "rng")
+  smp <- reactive(sample(rng(), input$b, replace = TRUE),
+                  label = "smp")
+  bc <- reactive(input$b * input$c,
+                 label = "bc")
+  
+  output$x <- renderPlot(hist(smp()))
+  output$y <- renderTable(max(smp()))
+  output$z <- renderText(bc())
 }
 
 # Run the application 
